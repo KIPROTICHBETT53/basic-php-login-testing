24f06832f0c424d34e3a7788234f718653ff1c00
ELLY BETT <bettelly84@gmail.com>
2024-11-03 11:42:34 +0300

    Update login.php

--- ecommerce_app/login.php
+++ ecommerce_app/login.php
@@ -4,7 +4,7 @@ require 'db_connection.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
-    $password = $_POST['passw'];
+    $password = $_POST['passwo'];
 
     // Use the correct column name from your database: "Password"
     $sql = "SELECT UserID, Password FROM Users WHERE Username = ?";
