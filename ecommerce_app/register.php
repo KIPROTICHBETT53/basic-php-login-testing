56623ca1e89860776c729e0bea64634f8792c0d5
ELLY BETT <161198100+KIPROTICHBETT53@users.noreply.github.com>
2024-11-04 11:18:16 +0300

    Update register.php

--- ecommerce_app/register.php
+++ ecommerce_app/register.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['userna'];
+    $username = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
 
